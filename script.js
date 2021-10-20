f86af1f8c685ed11b0e664fed0307c3772aa0a74
Nofi-nof <takenoko2402@gmail.com>
2021-10-20 14:03:57 +0900

    add data

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const { data } = require('jquery')
-
 //geolocation(位置情報取得)
 function success(pos) {
   ajaxRequest(pos.coords.latitude, pos.coords.longitude)
@@ -35,29 +33,30 @@ function ajaxRequest(lat, long) {
   })
     .done(function (data) {
       console.log(data)
+
+      //都市名・国名
+      console.log('都市名' + data.city.name)
+      console.log('国名' + data.city.country)
+
+      //天気予報データ
+      data.list.forEach(function (forecast, index) {
+        const dateTime = new Date(utcToJSTime(forecast.dt))
+        const month = dateTime.getMonth() + 1
+        const date = dateTime.getDate()
+        const hours = dateTime.getHours()
+        const min = String(dateTime.getMinutes()).padStart(2, '0')
+        const temperature = Math.round(forecast.main.temp)
+        const description = forecast.weather[0].description
+        const iconPath = `images/${forecast.weather[0].icon}.svg`
+
+        console.log('日時：' + '${month}/${date} ${hours}:${min}')
+        console.log('気温：' + temperature)
+        console.log('天気：' + description)
+        console.log('画像パス：' + iconPath)
+      })
     })
+
     .fail(function () {
       console.log('$.ajax failed!')
     })
-
-  //都市名・国名
-  console.log('都市名' + data.city.name)
-  console.log('国名' + data.city.country)
-
-  //天気予報データ
-  data.list.forEach(function (forecast, index) {
-    const dateTime = new Date(utcToJSTime(forecast.dt))
-    const month = dateTime.getMonth() + 1
-    const date = dateTime.getDate()
-    const hours = dateTime.getHours()
-    const min = String(dateTime.getMinutes()).padStart(2, '0')
-    const temperature = Math.round(forecast.main.temp)
-    const description = forecast.weather[0].description
-    const iconPath = `images/${forecast.weather[0].icon}.svg`
-
-    console.log('日時：' + '${month}/${date} ${hours}:${min}')
-    console.log('気温：' + temperature)
-    console.log('天気：' + description)
-    console.log('画像パス：' + iconPath)
-  })
 }
