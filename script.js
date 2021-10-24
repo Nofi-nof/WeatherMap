d5678910087a54d3c3c77798a0f05aeeabe096d7
Nofi-nof <takenoko2402@gmail.com>
2021-10-25 00:35:24 +0900

    create table

--- script.js
+++ script.js
@@ -32,14 +32,15 @@ function ajaxRequest(lat, long) {
     },
   })
     .done(function (data) {
-      console.log(data)
+      //console.log(data)
 
       //都市名・国名
-      console.log('都市名' + data.city.name)
-      console.log('国名' + data.city.country)
+      //console.log('都市名' + data.city.name)
+      //console.log('国名' + data.city.country)
+      $('#place').text(data.city.name + ',' + data.city.country)
 
       //天気予報データ
-      data.list.forEach((forecast) => {
+      data.list.forEach((forecast, index) => {
         const dateTime = new Date(utcToJSTime(forecast.dt))
         const month = dateTime.getMonth() + 1
         const date = dateTime.getDate()
@@ -47,12 +48,36 @@ function ajaxRequest(lat, long) {
         const min = String(dateTime.getMinutes()).padStart(2, '0')
         const temperature = Math.round(forecast.main.temp)
         const description = forecast.weather[0].description
-        const iconPath = `images/${forecast.weather[0].icon}.svg`
+        const iconPath = `images/${forecast.weather[0].icon}.png`
 
-        console.log('日時：' + `${month}/${date} ${hours}:${min}`)
-        console.log('気温：' + temperature)
-        console.log('天気：' + description)
-        console.log('画像パス：' + iconPath)
+        //console.log('日時：' + `${month}/${date} ${hours}:${min}`)
+        //console.log('気温：' + temperature)
+        //console.log('天気：' + description)
+        //console.log('画像パス：' + iconPath)
+
+        //現在の天気とそれ以外で出力を変える
+        if (index === 0) {
+          const currentWeather = `
+            <div class="icon"><img src="${iconPath}"></div>
+            <div class="info">
+              <p>
+                <span class="description">現在の天気：${description}</span>
+                <span class="temp">${temperature}</span>℃
+              </p>
+            </div>`
+          $('#weather').html(currentWeather)
+        } else {
+          const tableRow = `
+          <tr>
+            <td class="info>
+            ${month}/${date} ${hours}:${min}
+            </td>
+            <td class="icon"><img src="${iconPath}"></td>
+            <td><span class="description">${description}</span></td>
+            <td><span class="temp">${temperature}℃</span></td>
+          </tr>`
+          $('#forecast').append(tableRow)
+        }
       })
     })
 
